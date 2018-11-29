c489fc6458fadc63ff993529b42a339310a72e68
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-11-29 16:11:10 -0500

    Updates

---
+++ precache-manifest.4da520c42d2aca37787033c4f3ed8f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes,/static/media/pokeball.f10bf078.png"
+  },
+  {
+    "revision": "0a2a9970ad87eddfe01c",
+    "url": "/poketimes,/static/js/runtime~main.0a2a9970.js"
+  },
+  {
+    "revision": "9eb087c533e32232331e",
+    "url": "/poketimes,/static/js/main.9eb087c5.chunk.js"
+  },
+  {
+    "revision": "ef439eccfa9823c33453",
+    "url": "/poketimes,/static/js/1.ef439ecc.chunk.js"
+  },
+  {
+    "revision": "9eb087c533e32232331e",
+    "url": "/poketimes,/static/css/main.ab6e394a.chunk.css"
+  },
+  {
+    "revision": "9d15ab458825063a60d440c10b524f8e",
+    "url": "/poketimes,/index.html"
+  }
+];
