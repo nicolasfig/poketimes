0ef64afa8926463cbb20ebf47160193100da3d83
Nicolas Figueroa <nicolasfigb@gmail.com>
2018-11-28 21:07:37 -0500

    Finished

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nisi incidunt repellendus eveniet doloribus reiciendis voluptatibus vitae veritatis omnis earum itaque sequi enim, obcaecati perspiciatis dolore iure quidem aut sapiente!</p>
+        </div>
+    )
+}
+export default Rainbow(About);
